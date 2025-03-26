ea1418a4f8cc1bb761aabc4a582b31bec0ba5292
Yovan23 <157392939+Yovan23@users.noreply.github.com>
2025-03-26 17:01:05 +0530

    Added 4 cards in dashboaed

---
+++ src/app/services/dashboard.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {  Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { getAuthHeaders } from "../auth/auth-header";
+import { ApiResponse } from "../models/apiResponse.model";
+
+
+@Injectable
+({
+    providedIn: 'root'
+})
+export class DashboardService {
+    private apiUrl =  `${environment.baseUrl}/summary`;
+    authService: any;
+
+    constructor(private http: HttpClient) {}
+
+    summaryForAdmin(params?: { startDate?: string; endDate?: string}): Observable<ApiResponse> {
+        let httpParams = new HttpParams();
+        if (params) {
+            if (params.startDate) {
+                httpParams = httpParams.set('startDate', params.startDate);
+            }
+            if (params.endDate) {
+                httpParams = httpParams.set('endDate', params.endDate);
+            }
+        }
+        return this.http.post<ApiResponse>(`${this.apiUrl}/summaryForAdmin`, {
+          headers: getAuthHeaders(),
+          params: httpParams
+        })
+      }
+}    
