482a8b3bd4b9a7f0b15ffac2eb926adc631de76c
shashwat patel <shashwatpatel04@gmail.com>
2025-03-26 15:04:25 +0530

    dashboard update icon color

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,10 +2,42 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  stats = [
+    {
+      title: 'Orders',
+      value: '152',
+      icon: 'fa-shopping-cart',
+      change: '24 new',
+      changeText: 'since last visit',
+      iconColor: '#3b82f6',
+    },
+    {
+      title: 'Revenue',
+      value: '$2,100',
+      icon: 'fa-dollar-sign',
+      change: '52%+',
+      changeText: 'since last week',
+      iconColor: '#f59e0b',
+    },
+    {
+      title: 'Customers',
+      value: '28,441',
+      icon: 'fa-users',
+      change: '520',
+      changeText: 'newly registered',
+      iconColor: '#06b6d4',
+    },
+    {
+      title: 'Comments',
+      value: '152 Unread',
+      icon: 'fa-comments',
+      change: '85',
+      changeText: 'responded',
+      iconColor: '#8b5cf6',
+    },
+  ];
 }
