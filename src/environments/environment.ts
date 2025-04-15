8e6079bd6eb5508fe51688bf98068282d00d7f48
shashwat patel <shashwatpatel04@gmail.com>
2025-04-15 17:46:46 +0530

    baseUrl change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-    production: false,
-    // baseUrl: 'http://192.168.1.126:8080/api' ,
-    // imageUrl: 'http://192.168.1.126:8080/',
-    
-    baseUrl: 'https://l7q6l5xs-8080.inc1.devtunnels.ms/api' ,
-    imageUrl: 'https://l7q6l5xs-8080.inc1.devtunnels.ms/',
+  production: false,
+  // baseUrl: 'http://192.168.1.126:8080/api' ,
+  // imageUrl: 'http://192.168.1.126:8080/',
 
-    //  baseUrl: 'http://localhost:8080/api' ,
-    //  imageUrl:'http://localhost:8080/',
-  };
+  baseUrl: 'https://backend-supermarket.vercel.app/api',
+  imageUrl: 'https://l7q6l5xs-8080.inc1.devtunnels.ms/',
+
+  //  baseUrl: 'http://localhost:8080/api' ,
+  //  imageUrl:'http://localhost:8080/',
+};
